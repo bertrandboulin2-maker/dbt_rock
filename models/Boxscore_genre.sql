8386064fe554e7960b68c440c2daf3603ba0bab7
Bertrand Boulin <bertrandboulin2@gmail.com>
2026-03-12 10:57:51 +0000

    chgmt

--- models/Boxscore_genre.sql
+++ models/Boxscore_genre.sql
@@ -7,12 +7,17 @@ FROM {{ source('projectrawdata', 'genre_artist_boxscore') }}
 GROUP BY Nom, Genre
 ORDER BY nbre_concert DESC)
 
-SELECT Nom, COUNT(*) AS nbre_concert, Genre, sum(Total_Tickets) as total_tickets
+SELECT REGEXP_REPLACE(
+            LOWER(TRIM(Nom)),
+            r'[^a-z0-9\s]',
+            ''
+        ) AS artist_name_clean, 
+        COUNT(*) AS nbre_concert, 
+        Genre, 
+        sum(Total_Tickets) as total_tickets, 
 from table1
 join {{ source('projectrawdata', 'box_scores_all') }}
 on Artist = Nom
 group by Nom, Genre
 order by nbre_concert desc
-
-
 
